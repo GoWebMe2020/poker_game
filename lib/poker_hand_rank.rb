a7a6a7a09296dc93b3fe4c30e9c25f355d5bd827
GoWebMe <jpferreira1982@gmail.com>
2021-08-24 17:37:44 +0100

    Checking and returning hand outcomes

--- lib/poker_hand_rank.rb
+++ lib/poker_hand_rank.rb
@@ -1,5 +1,29 @@
 class PokerHandRank
 
+  def initialize
+    @poker_hand_outcome = ''
+  end
+
+  def poker_hand_rank(current_hand)
+    if is_a_straight_flush?(current_hand)
+      @poker_hand_outcome = 'Straight Flush'
+    elsif is_a_flush?(current_hand)
+      @poker_hand_outcome = 'Flush'
+    elsif is_a_straight?(current_hand)
+      @poker_hand_outcome = 'Straight'
+    elsif is_four_of_a_kind?(current_hand)
+      @poker_hand_outcome = 'Four of a Kind'
+    elsif is_three_of_a_kind?(current_hand)
+      @poker_hand_outcome = 'Three of a Kind'
+    elsif is_two_of_a_pair?(current_hand)
+      @poker_hand_outcome = 'Two Pair'
+    elsif is_a_pair?(current_hand)
+      @poker_hand_outcome = 'A Pair'
+    else
+      @poker_hand_outcome = 'High Card'
+    end
+  end
+
   def is_a_flush?(current_hand)
     suites = current_hand.map { |card| card[-1] }
     return false if suites.uniq.length > 1
@@ -13,12 +37,14 @@ class PokerHandRank
   
   def is_four_of_a_kind?(current_hand)
     card_values = current_hand.map { |card| card.size === 3 ? card[0..1].to_i : card[0].to_i }
-    card_values.uniq.length === 2
+    four_of_a_kind = card_values.detect{ |value| card_values.count(value) === 4}
+    four_of_a_kind != nil
   end
 
   def is_three_of_a_kind?(current_hand)
     card_values = current_hand.map { |card| card.size === 3 ? card[0..1].to_i : card[0].to_i }
-    card_values.uniq.length === 3 && !is_four_of_a_kind?(current_hand)
+    three_of_a_kind = card_values.detect{ |value| card_values.count(value) === 3}
+    three_of_a_kind != nil && !is_four_of_a_kind?(current_hand) ? true : false
   end
 
   def is_a_straight_flush?(current_hand)
@@ -27,7 +53,7 @@ class PokerHandRank
 
   def is_two_of_a_pair?(current_hand)
     card_values = current_hand.map { |card| card.size === 3 ? card[0..1].to_i : card[0].to_i }
-    card_values.uniq.length == 3
+    card_values.uniq.length === 3
   end
 
   def is_a_pair?(current_hand)
--- spec/poker_hand_rank_spec.rb
+++ spec/poker_hand_rank_spec.rb
@@ -84,4 +84,46 @@ describe PokerHandRank do
     @hand = ['7S', '7D', '10H', '8C', '9S']
     expect(@poker_hand_rank.is_two_of_a_pair?(@hand)).to eq(false)
   end
+
+  it 'returns a flush' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['2S', '5S', 'JS', '4S', '10S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Flush')
+  end
+
+  it 'returns a straight' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '6D', '10S', '8H', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Straight')
+  end
+
+  it 'returns a four of a kind' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '7D', '7H', '7C', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Four of a Kind')
+  end
+
+  it 'returns a three of a kind' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '7D', '7H', '3C', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Three of a Kind')
+  end
+
+  it 'returns a straight flush' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '6S', '10S', '8S', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Straight Flush')
+  end
+
+  it 'returns a two pair' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '7D', '10H', '10C', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('Two Pair')
+  end
+
+  it 'returns a a pair' do
+    @poker_hand_rank = PokerHandRank.new
+    @hand = ['7S', '7D', '10H', '3C', '9S']
+    expect(@poker_hand_rank.poker_hand_rank(@hand)).to eq('A Pair')
+  end
 end
