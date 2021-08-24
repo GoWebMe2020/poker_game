ed34cca538820b355c7f76ea45e3bcd55fc02cbe
GoWebMe <jpferreira1982@gmail.com>
2021-08-24 12:11:37 +0100

    Refactoring tests

--- spec/poker_spec.rb
+++ spec/poker_spec.rb
@@ -1,19 +1,21 @@
 require 'poker'
 
 describe Poker do
-  it 'is an instance of Poker' do
+
+  before(:each) do
     @poker = Poker.new
+  end
+
+  it 'is an instance of Poker' do
     expect(@poker).to be_an_instance_of(Poker)
   end
 
   it 'can draw a hand of 5 cards from the deck' do
-    @poker = Poker.new
     @poker.draw
     expect(@poker.hand.length).to eq(5)
   end
 
   it 'can determine if there are duplicate cards' do
-    @poker = Poker.new
     @poker.draw
     expect(@poker.hand.uniq.length).to eq(5)
   end
