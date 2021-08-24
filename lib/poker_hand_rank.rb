14d09385d6286a9ded8bac3d7c1b337bba0ce056
GoWebMe <jpferreira1982@gmail.com>
2021-08-24 20:41:21 +0100

    Checking for a Royal Flush

--- lib/poker_hand_rank.rb
+++ lib/poker_hand_rank.rb
@@ -5,7 +5,9 @@ class PokerHandRank
   end
 
   def poker_hand_rank(current_hand)
-    if is_a_straight_flush?(current_hand)
+    if is_a_royal_flush(current_hand)
+      @poker_hand_outcome = 'Royal Flush'
+    elsif is_a_straight_flush?(current_hand)
       @poker_hand_outcome = 'Straight Flush'
     elsif is_a_flush?(current_hand)
       @poker_hand_outcome = 'Flush'
@@ -13,7 +15,7 @@ class PokerHandRank
       @poker_hand_outcome = 'Straight'
     elsif is_four_of_a_kind?(current_hand)
       @poker_hand_outcome = 'Four of a Kind'
-    elsif is_a_full_house(current_hand)
+    elsif is_a_full_house?(current_hand)
       @poker_hand_outcome = 'Full House'
     elsif is_three_of_a_kind?(current_hand)
       @poker_hand_outcome = 'Three of a Kind'
@@ -42,7 +44,7 @@ class PokerHandRank
     numbered_hand = convert_card_values(current_hand)
     card_values = numbered_hand.map { |card| card.size === 3 ? card[0..1].to_i : card[0].to_i }
     four_of_a_kind = card_values.detect{ |value| card_values.count(value) === 4}
-    four_of_a_kind != nil && !is_a_full_house(current_hand)
+    four_of_a_kind != nil && !is_a_full_house?(current_hand)
   end
 
   def is_three_of_a_kind?(current_hand)
@@ -68,11 +70,20 @@ class PokerHandRank
     card_values.uniq.length === 4
   end
 
-  def is_a_full_house(current_hand)
-    if (is_a_pair?(current_hand)) && (is_three_of_a_kind?(current_hand))
-      true
+  def is_a_full_house?(current_hand)
+    numbered_hand = convert_card_values(current_hand)
+    card_values = numbered_hand.map { |card| card.size === 3 ? card[0..1].to_i : card[0].to_i }
+    three_of_a_kind = card_values.detect{ |value| card_values.count(value) === 3}
+    pair = card_values.detect{ |value| card_values.count(value) === 2}
+    pair != nil && three_of_a_kind != nil
+  end
+
+  def is_a_royal_flush(current_hand)
+    numbered_hand = convert_card_values(current_hand)
+    if (numbered_hand.max === 14) && (is_a_straight_flush?(current_hand))
+      return true
     else
-      false
+      return false
     end
   end
 
